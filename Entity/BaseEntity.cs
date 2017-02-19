51df6d2683a769c69220cef4f27c632c714d1945
Hao Qian <hao.qian@mainpac.com.au>
2017-02-20 09:42:58 +1100

    Update readme

--- Entity/BaseEntity.cs
+++ Entity/BaseEntity.cs
@@ -2,6 +2,9 @@
 
 namespace XeroTechnicalTest
 {
+    /// <summary>
+    /// Base class for all entities. Common properties go here.
+    /// </summary>
     public class BaseEntity
     {
         public DateTime CreateDateTime { get; set; }
